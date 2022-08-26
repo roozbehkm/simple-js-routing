93adab28ec5bb8543bad8208d152d5886c5dd625
roozbeh.1271@gmail.com <roozbeh.1271@gmail.com>
2022-08-26 18:55:03 +0430

    JS Routing

---
+++ app.js
@@ -0,0 +1,51 @@
+let mainTitle = '';
+
+async function loadPage(pageName) {
+        const resp = await fetch(`pages/${pageName}.html`)
+        
+        if (resp.status === 404) {
+            loadPage(404);
+            return;
+        }
+
+        const html = await resp.text();
+        const dom = new DOMParser();
+        const page = dom.parseFromString(html, 'text/html');
+
+        document.getElementById('app-content').innerHTML = page.body.innerHTML;
+        // document.head.innerHTML = page.head.innerHTML;
+        // document.title = `${mainTitle} - ${page.title}`;
+        document.title = page.title ? `${mainTitle} - ${page.title}` : mainTitle;
+
+        window.history.pushState(null, null, `/${pageName}`);
+
+        changeLinksActions();
+}
+
+function changeLinksActions() {
+    document.querySelectorAll('a').forEach((el) => {
+        el.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            if (document.location.origin == e.target.origin) {
+                // for preventive measures :D
+                const pageName = e.target.href
+                    .replace(e.target.origin, '')
+                    .replace(/\.html/, '')
+                    .replace(/pages/, '')
+                    .replace(/^\//, '');
+                
+                loadPage(pageName);
+            } else {
+                document.location.href = e.target.href;
+            }
+        })
+    })
+}
+
+ function inti() {
+    mainTitle = document.title;
+    loadPage('a');
+ }
+ 
+ inti()
